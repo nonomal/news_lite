13da038c6b65c21c653807cda05f166dc9ff6b46
Dmitry <rps_project@mail.ru>
2021-12-22 15:33:21 +0300

    Create Sender.java

---
+++ src/main/java/com/news/Sender.java
@@ -0,0 +1,42 @@
+package com.news;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Sender {
+    private String user;
+    private String pwd;
+    private Properties p;
+
+    public Sender(String username, String password) {
+        this.user = username;
+        this.pwd = password;
+
+        p = new Properties();
+        p.put("mail.smtp.host", "smtp.mail.ru");
+        p.put("mail.smtp.socketFactory.port", 465);
+        p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        p.put("mail.smtp.auth", "true");
+        p.put("mail.smtp.port", 465);
+    }
+
+    public void send(String subject, String text, String fromEmail, String toEmail){
+        Session session = Session.getDefaultInstance(p, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pwd);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
