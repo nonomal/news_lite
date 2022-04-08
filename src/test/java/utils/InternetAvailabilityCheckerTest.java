aa744efae3d2f82d837537185845fca15a4c9c30
Avandy <rps_project@mail.ru>
2022-04-08 12:39:01 +0300

    feat: тест подключения к SQLite

--- src/test/java/utils/InternetAvailabilityCheckerTest.java
+++ src/test/java/utils/InternetAvailabilityCheckerTest.java
@@ -1,11 +1,15 @@
 package utils;
 
+import main.Main;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +28,17 @@ class InternetAvailabilityCheckerTest {
                     () -> socket.connect(new InetSocketAddress(hostName, port), 3000),
                     "Не выброшено исключение о том, что нет подключения к интернету");
         }
+    }
+
+    @Test
+    public void shouldOpenSQLiteConnectionTest() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + Main.directoryPath + "news.db");
+            assertFalse(connection.isClosed());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
