519b6d3263cc1143a18c3393d0aee1a80ca1cf14
Avandy <rps_project@mail.ru>
2022-05-22 17:06:32 +0300

    feat: log

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -40,7 +40,7 @@ public class Main {
         if (!mainDirectory.exists()) mainDirectory.mkdirs();
 
         // log file create
-//        File logIsExists = new File(DIRECTORY_PATH + "app.log");
+//        File logIsExists = new File(DIRECTORY_PATH + "app.log"); // TODO заполнять логом файл по этому адресу
 //        if (!logIsExists.exists()) {
 //            try {
 //                logIsExists.createNewFile();
