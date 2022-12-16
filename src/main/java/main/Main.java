bb0ec67fbae12a9efb4da3b4e314ee4d57005c4a
Dmitry <rps_project@mail.ru>
2022-12-16 14:26:38 +0300

    info: login

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import database.JdbcQueries;
 import database.SQLite;
+import gui.buttons.Icons;
 import search.ConsoleSearch;
 import utils.Common;
 import utils.Reminder;
@@ -16,15 +17,30 @@ public class Main {
     public static void main(String[] args) {
         Common.createFiles();
         new SQLite().openConnection();
-        login(args);
+
+        if (args.length == 0) {
+            Common.showGui();
+            new Reminder().remind();
+            login();
+        } else {
+            /*
+              main.Main arguments for console search:
+              args1 = email
+              args2 = interval in minutes
+              args3 = keyword1, keyword2 ... argsN = search keywords
+            */
+            new ConsoleSearch().searchByConsole(args);
+        }
     }
 
-    private static void login(String[] args) {
+    private static void login() {
+        JdbcQueries jdbcQueries = new JdbcQueries();
         String[] loginParams = showLoginDialog();
 
-        if (Integer.parseInt(loginParams[0]) == 0) {
+        int option = Integer.parseInt(loginParams[0]);
+
+        if (option == 0) {
             username = loginParams[1];
-            JdbcQueries jdbcQueries = new JdbcQueries();
 
             if (jdbcQueries.isUserExists(username)) {
                 userId = jdbcQueries.getUserIdByUsername(username);
@@ -32,41 +48,33 @@ public class Main {
                 String userHashPassword = jdbcQueries.getUserHashPassword(username);
 
                 // Correct password
-                if (password.equals(userHashPassword)) {
-                    if (args.length == 0) {
-                        Common.showGui();
-                        new Reminder().remind();
-                    } else {
-                        /*
-                          main.Main arguments for console search:
-                          args1 = email
-                          args2 = interval in minutes
-                          args3 = keyword1, keyword2 ... argsN = search keywords
-                        */
-                        new ConsoleSearch().searchByConsole(args);
-                    }
+                if (password.equals(userHashPassword) || username.equals("default")) {
+                    System.out.println("Ok");
                 } else {
                     JOptionPane.showMessageDialog(null, "Incorrect password");
-                    login(args);
+                    login();
                 }
             } else {
-                JOptionPane.showMessageDialog(null, "User not found, set to default settings");
-                username = "default";
-                userId = 0;
+                JOptionPane.showMessageDialog(null, "User not found");
+                login();
             }
-
-        } else {
+        } else if (option == 1) {
+            username = "default";
+            userId = 0;
+        } else if (option == 2) {
+            //jdbcQueries.createUser();
+        } else if (option == 3) {
             System.exit(0);
         }
     }
 
     /*
-     login[0] = Ok or Exit from dialog
+     login[0] = Ok, Default user, Create user, Exit from dialog
      login[1] = username
      login[2] = password
     */
     private static String[] showLoginDialog() {
-        String[] login = new String[3];
+        String[] options = new String[3];
 
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(4, 2, 0, 5));
@@ -79,15 +87,15 @@ public class Main {
         panel.add(passwordLabel);
         panel.add(passwordField);
 
-        String[] options = new String[]{"Ok", "Exit"};
+        String[] menu = new String[]{"Ok", "Default user", "Create user", "Exit"};
         int option = JOptionPane.showOptionDialog(null, panel, "Login",
                 JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, options, options[0]);
+                Icons.LOGO_ICON, menu, menu[0]);
 
-        login[0] = String.valueOf(option);
-        login[1] = user.getText();
-        login[2] = Common.getHash(new String(passwordField.getPassword()));
+        options[0] = String.valueOf(option);
+        options[1] = user.getText();
+        options[2] = Common.getHash(new String(passwordField.getPassword()));
 
-        return login;
+        return options;
     }
 }
