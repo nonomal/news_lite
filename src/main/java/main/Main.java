c48f1c5deb36fd1e591d8f729c38c6823d1f3d83
Dmitry <rps_project@mail.ru>
2022-12-16 12:10:43 +0300

    info: login

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,46 +16,39 @@ public class Main {
     public static void main(String[] args) {
         Common.createFiles();
         new SQLite().openConnection();
-        login();
-
-        if (args.length == 0) {
-            Common.showGui();
-            new Reminder().remind();
-        } else {
-            /*
-              main.Main arguments for console search:
-              args1 = email
-              args2 = interval in minutes
-              args3 = keyword1, keyword2 ... argsN = search keywords
-             */
-            new ConsoleSearch().searchByConsole(args);
-        }
+        login(args);
     }
 
-    private static void login() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 2, 0, 5));
-        JLabel userLabel = new JLabel("user:");
-        JTextField user = new JTextField("default"); //avandy
-        //JLabel passwordLabel = new JLabel("password:");
-        //JPasswordField password = new JPasswordField(10);
-        panel.add(userLabel);
-        panel.add(user);
-        //panel.add(passwordLabel);
-        //panel.add(password);
+    private static void login(String[] args) {
+        String[] loginParams = showLoginDialog();
 
-        String[] options = new String[]{"Ok", "Exit"};
-        int option = JOptionPane.showOptionDialog(null, panel, "Login",
-                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                null, options, options[0]);
+        if (Integer.parseInt(loginParams[0]) == 0) {
+            username = loginParams[1];
+            JdbcQueries jdbcQueries = new JdbcQueries();
 
-        if(option == 0) {
-            username = user.getText();
+            if (jdbcQueries.isUserExists(username)) {
+                userId = jdbcQueries.getUserIdByUsername(username);
+                String password = loginParams[2];
+                String userHashPassword = jdbcQueries.getUserHashPassword(username);
 
-            if (new JdbcQueries().isUserExists(username)) {
-                userId = new JdbcQueries().getUserIdByUsername(username);
-                System.out.println("Your user: " + username + " user_id = " + userId);
-                //char[] pwd = password.getPassword();
+                // Correct password
+                if (password.equals(userHashPassword)) {
+                    if (args.length == 0) {
+                        Common.showGui();
+                        new Reminder().remind();
+                    } else {
+                        /*
+                          main.Main arguments for console search:
+                          args1 = email
+                          args2 = interval in minutes
+                          args3 = keyword1, keyword2 ... argsN = search keywords
+                        */
+                        new ConsoleSearch().searchByConsole(args);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect password");
+                    showLoginDialog();
+                }
             } else {
                 JOptionPane.showMessageDialog(null, "User not found, set to default settings");
                 username = "default";
@@ -65,6 +58,36 @@ public class Main {
         } else {
             System.exit(0);
         }
+    }
 
+    /*
+     login[0] = Ok or Exit from dialog
+     login[1] = username
+     login[2] = password
+    */
+    private static String[] showLoginDialog() {
+        String[] login = new String[3];
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2, 0, 5));
+        JLabel userLabel = new JLabel("Username");
+        JTextField user = new JTextField("avandy"); // avandy default
+        JLabel passwordLabel = new JLabel("Password");
+        JPasswordField passwordField = new JPasswordField();
+        panel.add(userLabel);
+        panel.add(user);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+
+        String[] options = new String[]{"Ok", "Exit"};
+        int option = JOptionPane.showOptionDialog(null, panel, "Login",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        login[0] = String.valueOf(option);
+        login[1] = user.getText();
+        login[2] = Common.getHash(new String(passwordField.getPassword()));
+
+        return login;
     }
 }
